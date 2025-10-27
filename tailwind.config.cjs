c7997d28c38ff6b8b6bf427f9f791e16fd99e5e1
LauraCastrillonMp <lauracastrillon.mp@gmail.com>
2025-10-27 01:11:47 -0500

    Add Tailwind CSS configuration file for styling

---
+++ tailwind.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    './app/**/*.{ts,tsx,js,jsx,css}',
+    './components/**/*.{ts,tsx,js,jsx,css}',
+    './pages/**/*.{ts,tsx,js,jsx,css}',
+  ],
+  theme: { extend: {} },
+  plugins: [],
+};
